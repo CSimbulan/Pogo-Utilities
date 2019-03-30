8ecb81504c6b3e83ca95af05046fa5312170af49
CSimbulan <c.simbulan@hotmail.com>
2019-03-29 21:18:59 -0400

    Able to match input to basestats table

--- calculate.js
+++ calculate.js
@@ -1,22 +1,62 @@
 /*This function happens when the page loads.*/
-/*Links the calculate function to the button.*/
 window.onload = function()
 {
+    /*Links the calculate function to the button.*/
 	var addButton = document.getElementById("calculate");
-	addButton.onclick = calculate;
+    addButton.onclick = calculate;
+    
+    /*Loads the base stats file and stores into a global.*/
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            window.basestats = this.responseText.split("\n");
+        }
+    }
+    xhttp.open("GET", "basestats.txt", true);
+    xhttp.send();
 }
 
 /*Calculates the CPs and IVs*/
 function calculate() {
+
+    /*Get elements from HTML doc and create new table.*/
     var output = document.getElementById("output");
     var input = document.getElementById("name");
     var newT = document.createElement("table");
     output.appendChild(newT);
+
+    /*Clear the output.*/
+    outputP = output.getElementsByTagName("p");
+    if (outputP.length > 0) {
+        output.removeChild(outputP[outputP.length-1]);
+    }
+
+    /*Match input string to base stats*/
+    var name = input.value.toLowerCase();
+    for (var i=0; i<window.basestats.length; i++) {
+        var line = window.basestats[i].split(",");
+        if (name == line[0].toLowerCase()) {
+            var atk = parseInt(line[1]);
+            var def = parseInt(line[2]);
+            var sta = parseInt(line[3]);
+            /*
+            var newp = document.createElement("p");
+            newp.innerHTML = line + name + atk + def + sta;
+            output.appendChild(newp); */
+        }
+    }
+
+    /*Table generator.*/
     for (var a=0; a<10; a++) {
         var row = newT.insertRow(a);
         for (var b=0; b<10; b++) {
             var cell = row.insertCell(b);
-            cell.innerHTML = a + "," + b;
+            cell.innerHTML = atk + "," + def;
         }
     }
+
+
+
+
+
 }
