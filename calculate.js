b1c3395335732f9539e87274fcf50e6623df6895
CSimbulan <c.simbulan@hotmail.com>
2019-03-30 15:42:19 -0400

    cp/iv chart functioning for lvl20,25 and sorted by cp

--- calculate.js
+++ calculate.js
@@ -20,9 +20,10 @@ window.onload = function()
 function calculate() {
 
     /*Get elements from HTML doc and create new table.*/
-    output = document.getElementById("output");
-    input = document.getElementById("name");
-    table = document.getElementById("ivtable");
+    var output = document.getElementById("output");
+    var input = document.getElementById("name");
+    var table = document.getElementById("ivtable");
+    var statsArray = [];
 
     /*Clear the table*/
     table.innerHTML = "";
@@ -42,17 +43,48 @@ function calculate() {
         }
     }
 
-    /*Table generator.*/
-    for (var a=0; a<10; a++) {
-        var row = table.insertRow(a);
-        for (var b=0; b<10; b++) {
-            var cell = row.insertCell(b);
-            cell.innerHTML = atk + "," + def;
+    for (var at=15; at>9; at--) {
+        for (var de=15; de>9; de--) {
+            for (var st=15; st>9; st--) {
+                var iv = (at + de + st) / 45.0 * 100;
+                var cp20 = Math.floor((atk + at) * Math.sqrt(def + de) * Math.sqrt(sta + st) * Math.pow(0.59740001,2) / 10);
+                var cp25 = Math.floor((atk + at) * Math.sqrt(def + de) * Math.sqrt(sta + st) * Math.pow(0.667934,2) / 10);
+                var cp40 = Math.floor((atk + at) * Math.sqrt(def + de) * Math.sqrt(sta + st) * Math.pow(0.79030001,2) / 10);
+                var hp20 = Math.floor((sta + st) * 0.59740001);
+                var hp25 = Math.floor((sta + st) * 0.667934);
+                var hp40 = Math.floor((sta + st) * 0.79030001);
+                var newArray = [cp20, hp20, cp25, hp25, iv.toFixed(1), at, de, st, cp40, hp40];
+                statsArray.push(newArray);
+            }
         }
     }
 
+    statsArray.sort(sortFunction);
+
+    /*Table generator.*/
+    var row = table.insertRow(0);
+    row.innerHTML = "<th>CP@20</th><th>HP@20</th><th>CP@25<th>HP@25</th><th>IV%</th><th>ATT</th><th>DEF</th><th>STA</th><th>CP@40</th><th>HP@40</th>";
+
+    for (var a=0; a<statsArray.length; a++) {
+        row = table.insertRow(a+1);
+        for (var b=0; b<statsArray[0].length; b++) {
+            var cell = row.insertCell(b);
+            cell.innerHTML = statsArray[a][b];
+        }
+    }
+
+    
 
 
 
 
+}
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] > b[0]) ? -1 : 1;
+    }
 }
